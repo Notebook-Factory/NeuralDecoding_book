ec6fd50a2b6eb5bee7e4b0bff6475357235a5f09
Kiril Zelenkovski <zelenkastiot@gmail.com>
2020-07-06 00:39:51 +0200

    Add figure 5

--- content/03/subsection/02-04/ratbilstm.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-label=$1
-shift
-#nohup srun --partition=gpu --gres=gpu:1 --constraint=K20 python ratbilstm.py $label.hdf5 --verbose 2 $* >$label.log &
-nohup python ratbilstm.py $label.hdf5 --verbose 2 $* >$label.log
--- content/03/subsection/02-04/ratcvfit.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-label=$1
-shift
-#nohup srun --partition=gpu --gres=gpu:1 --constraint=K20 python ratcvfit.py $label --verbose 2 $* >$label.log &
-nohup python ratcvfit.py $label --verbose 2 $* >$label.log
